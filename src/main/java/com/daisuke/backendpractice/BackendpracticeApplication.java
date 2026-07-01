c19663b7b748b2edc3db5fa0811ecb6ce976ead4
Daisuke <dk.joker.1107@icloud.com[credential]>
2026-07-01 11:37:52 +0900

    Initial Spring Boot project

---
+++ src/main/java/com/daisuke/backendpractice/BackendpracticeApplication.java
@@ -0,0 +1,14 @@
+package com.daisuke.backendpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendpracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendpracticeApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/daisuke/backendpractice/BackendpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daisuke.backendpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendpracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
