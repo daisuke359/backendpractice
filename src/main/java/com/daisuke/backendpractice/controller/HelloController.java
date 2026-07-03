e99ec9f9d8d81f3ac20834f5a806680bffe796bb
Daisuke <dk.joker.1107@icloud.com[credential]>
2026-07-03 23:02:31 +0900

    feat: add DELETE /students endpoint

--- src/main/java/com/daisuke/backendpractice/controller/HelloController.java
+++ src/main/java/com/daisuke/backendpractice/controller/HelloController.java
@@ -1,11 +1,8 @@
 package com.daisuke.backendpractice.controller;
 
 import com.daisuke.backendpractice.model.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -46,6 +43,15 @@ public class HelloController {
         return student;
     }
 
+    @DeleteMapping("/students/{id}")
+    public Student deleteStudent(@PathVariable int id) {
+        for(int i=0;i<students.size();i++) {
+            if(students.get(i).getId()==id) {
+                return students.remove(i);
+            }
+        }
+        return null;
+    }
 
 
 }
